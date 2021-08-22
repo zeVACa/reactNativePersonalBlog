4d7d0a727eb0a798baa5b1f64db45653ea006f63
Nikita Logunyonok <38005134+zeVACa@users.noreply.github.com>
2021-08-22 09:04:11 +0300

    basic layout

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const MainScreen = () => {
+  return (
+    <View>
+      <Text>MainScreen</Text>
+    </View>
+  );
+};
